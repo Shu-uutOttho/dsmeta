9b468efd9ed58eca21d7d5459242bd75e180d329
Shu <controvictor@gmail.com>
2022-11-14 19:21:21 -0300

    First Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,17 @@
+import NotificationButton from "./components/NotificationButton"
 
 
 function App() {
   return (
-    <h1>Olá mundo!</h1>
-    )
+    <>
+      <h1>Olá mundo!</h1>
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+    </>
+  )
 }
 
 export default App
