7e1226dbfffa5bd2caf8589e7096823050e801f3
Shu <controvictor@gmail.com>
2022-11-16 15:58:21 -0300

    Database

---
+++ backend/src/main/java/com/devsuperior/dsmeta/entities/Sale.java
@@ -0,0 +1,105 @@
+package com.devsuperior.dsmeta.entities;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_sales")
+public class Sale {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String SellerName;
+	private Integer visited;
+	private Integer deals;
+	private Double amount;
+	private LocalDate date;
+
+	public Sale() {
+	}
+
+	public Sale(Long id, String sellerName, Integer visited, Integer deals, Double amount, LocalDate date) {
+		super();
+		this.id = id;
+		SellerName = sellerName;
+		this.visited = visited;
+		this.deals = deals;
+		this.amount = amount;
+		this.date = date;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getSellerName() {
+		return SellerName;
+	}
+
+	public void setSellerName(String sellerName) {
+		SellerName = sellerName;
+	}
+
+	public Integer getVisited() {
+		return visited;
+	}
+
+	public void setVisited(Integer visited) {
+		this.visited = visited;
+	}
+
+	public Integer getDeals() {
+		return deals;
+	}
+
+	public void setDeals(Integer deals) {
+		this.deals = deals;
+	}
+
+	public Double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(SellerName, amount, date, deals, id, visited);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Sale other = (Sale) obj;
+		return Objects.equals(SellerName, other.SellerName) && Objects.equals(amount, other.amount)
+				&& Objects.equals(date, other.date) && Objects.equals(deals, other.deals)
+				&& Objects.equals(id, other.id) && Objects.equals(visited, other.visited);
+	}
+
+}
